2b478c765b901c9c8a13c9ec3d5bc4c9f8379928
quocdatk18 <quocdatlop109@gmail.com>
2025-08-03 22:53:16 +0700

    fix

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -4,6 +4,7 @@ import axios from 'axios';
 const axiosClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
   timeout: 10000, // 10s timeout mặc định
+  withCredentials: true,
 });
 
 // ===== Interceptor: Tự động gắn token vào header =====
